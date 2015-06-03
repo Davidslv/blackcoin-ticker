f5aa4d1f1b66f29331dc29b6f13978d414e54a02
David Silva <davidslv@gmail.com>
2015-06-04 00:43:48 +0100

    Trying to fix a problem with rack

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 require './response_builder'
 
-get '/' do
-  content_type :json
-  ResponseBuilder.new.deliver
+class BlackcoinTickerApp < Sinatra::Base
+  set :protection, :except => [:json_csrf]
+
+  def initialize(*args)
+    super
+  end
+
+  get '/' do
+    content_type :json
+    ResponseBuilder.new.deliver
+  end
 end
